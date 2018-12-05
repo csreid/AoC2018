9415bfa42c09babaeca2e121acf7eef91bd6b7fb
Cameron ReidA <csreid.cr@gmail.com>
2018-12-05 12:40:25 -0500

    Really day 2 part 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,8 +1,16 @@
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::ops::Sub;
+use std::ops::BitAnd;
+use std::fmt::{Display,Result,Formatter};
 
-#[derive(Debug,Deserialize,Clone,Hash,PartialEq,Eq)]
+#[derive(Debug,Deserialize,Clone,Hash,PartialEq,Eq,PartialOrd,Ord)]
 pub struct BoxID (String);
+pub struct BoxDiff {
+	position: i32,
+	left: char,
+	right: char
+}
 
 impl BoxID {
 	pub fn new(s:String) -> BoxID {
@@ -17,6 +25,50 @@ impl BoxID {
 			*tmp += 1;
 		};
 
-		chars.values().filter(|x| **x == n).collect::<Vec<&i32>>().len() > 0
+		chars
+			.values()
+			.filter(|x| **x == n)
+			.collect::<Vec<&i32>>()
+			.len() > 0
+	}
+}
+
+impl Sub<BoxID> for BoxID {
+	type Output = Vec<BoxDiff>;
+
+	fn sub(self, other:BoxID) -> Self::Output {
+		self.0.chars()
+			.zip(other.0.chars())
+			.enumerate()
+			.filter(|(idx, (l, r))| {
+				l != r
+			})
+			.map(|(idx, (l, r))| BoxDiff {
+				position: idx as i32,
+				left: l,
+				right: r
+			})
+			.collect::<Vec<BoxDiff>>()
+	}
+}
+
+impl BitAnd<BoxID> for BoxID {
+	type Output = String;
+
+	fn bitand(self, other:BoxID) -> Self::Output {
+		self.0.chars()
+			.zip(other.0.chars())
+			.filter(|(l, r)| {
+				l == r
+			})
+			.map(|(_, r)| r)
+			.collect::<String>()
+	}
+}
+
+impl Display for BoxDiff {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		write!(f, "{} -> {} as position {}", self.left, self.right, self.position);
+		Ok(())
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate csv;
+#[macro_use]
 extern crate itertools;
 #[macro_use]
 extern crate serde_derive;
@@ -14,10 +15,16 @@ use std::convert::From;
 use clap::{Arg,App};
 use fine_grained::Stopwatch;
 use day1::{Change,ChangeRecord,ChangeList};
-use day2::{BoxID};
+use day2::{BoxID, BoxDiff};
+
+#[derive(Debug)]
+enum ResultValue {
+	S(String),
+	Num(i32)
+}
 
 
-fn process(day:i32, part:i32) -> i32 {
+fn process(day:i32, part:i32) -> ResultValue {
 	let mut rdr = csv::ReaderBuilder::new()
 		.has_headers(false)
 		.from_reader(io::stdin());
@@ -28,8 +35,8 @@ fn process(day:i32, part:i32) -> i32 {
 			let cl = ChangeList::new(input);
 
 			match part {
-				1 => cl.result(),
-				2 => cl.first_dup(),
+				1 => ResultValue::Num(cl.result()),
+				2 => ResultValue::Num(cl.first_dup()),
 				_ => panic!("Invalid part")
 			}
 		},
@@ -41,10 +48,26 @@ fn process(day:i32, part:i32) -> i32 {
 					let twos:Vec<BoxID> = input.clone().into_iter().filter(|x| x.exactly_n(2)).collect();
 					let threes:Vec<BoxID> = input.clone().into_iter().filter(|x| x.exactly_n(3)).collect();
 
-					(twos.len() * threes.len()) as i32
+					ResultValue::Num((twos.len() * threes.len()) as i32)
 				},
 
-				2 => unimplemented!(),
+				2 => {
+					let ids = input.clone();
+					let ids2 = input.clone();
+
+					let result = iproduct!(ids, ids2)
+						.map(|(l, r)| {
+							(l.clone(), r.clone(), l.clone() - r.clone())
+						})
+						.filter(|(l, r, v)| {
+							v.len() == 1
+						})
+						.collect::<Vec<(BoxID, BoxID, Vec<BoxDiff>)>>();
+
+					assert!(result.len() == 2);
+
+					ResultValue::S(result[0].0.clone() & result[0].1.clone())
+				},
 				_ => panic!("Invalid part")
 			}
 		}
@@ -92,6 +115,6 @@ fn main() {
 	let mut stopwatch = Stopwatch::start_new();
 	let result = process(day, part);
 	let time = stopwatch.lap();
-	println!("{} in {}ns", result, time);
+	println!("{:?} in {}ns", result, time);
 	stopwatch.stop();
 }
